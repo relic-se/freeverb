03ddd0a6534a6a85c0d9cfdf399eac9ad24e1460
dcooperdalrymple <dcooperdalrymple@gmail.com>
2025-01-30 14:49:46 -0600

    Allow filter tuning based on sample rate and a variable number of allpass and comb filters.

--- Components/revmodel.cpp
+++ Components/revmodel.cpp
@@ -5,44 +5,19 @@
 // This code is public domain
 
 #include "revmodel.hpp"
+#include <stdlib.h>
 
 revmodel::revmodel()
 {
-	// Tie the components to their buffers
-	combL[0].setbuffer(bufcombL1,combtuningL1);
-	combR[0].setbuffer(bufcombR1,combtuningR1);
-	combL[1].setbuffer(bufcombL2,combtuningL2);
-	combR[1].setbuffer(bufcombR2,combtuningR2);
-	combL[2].setbuffer(bufcombL3,combtuningL3);
-	combR[2].setbuffer(bufcombR3,combtuningR3);
-	combL[3].setbuffer(bufcombL4,combtuningL4);
-	combR[3].setbuffer(bufcombR4,combtuningR4);
-	combL[4].setbuffer(bufcombL5,combtuningL5);
-	combR[4].setbuffer(bufcombR5,combtuningR5);
-	combL[5].setbuffer(bufcombL6,combtuningL6);
-	combR[5].setbuffer(bufcombR6,combtuningR6);
-	combL[6].setbuffer(bufcombL7,combtuningL7);
-	combR[6].setbuffer(bufcombR7,combtuningR7);
-	combL[7].setbuffer(bufcombL8,combtuningL8);
-	combR[7].setbuffer(bufcombR8,combtuningR8);
-	allpassL[0].setbuffer(bufallpassL1,allpasstuningL1);
-	allpassR[0].setbuffer(bufallpassR1,allpasstuningR1);
-	allpassL[1].setbuffer(bufallpassL2,allpasstuningL2);
-	allpassR[1].setbuffer(bufallpassR2,allpasstuningR2);
-	allpassL[2].setbuffer(bufallpassL3,allpasstuningL3);
-	allpassR[2].setbuffer(bufallpassR3,allpasstuningR3);
-	allpassL[3].setbuffer(bufallpassL4,allpasstuningL4);
-	allpassR[3].setbuffer(bufallpassR4,allpasstuningR4);
+	setstereo(true);
+	setsamplerate(44100);
+	reset();
 
 	// Set default values
-	allpassL[0].setfeedback(0.5f);
-	allpassR[0].setfeedback(0.5f);
-	allpassL[1].setfeedback(0.5f);
-	allpassR[1].setfeedback(0.5f);
-	allpassL[2].setfeedback(0.5f);
-	allpassR[2].setfeedback(0.5f);
-	allpassL[3].setfeedback(0.5f);
-	allpassR[3].setfeedback(0.5f);
+	for (int i=0;i<numallpasses;i++) {
+		allpassL[i].setfeedback(0.5f);
+		allpassR[i].setfeedback(0.5f);
+	}
 	setwet(initialwet);
 	setroomsize(initialroom);
 	setdry(initialdry);
@@ -52,6 +27,58 @@ revmodel::revmodel()
 
 	// Buffer will be full of rubbish - so we MUST mute them
 	mute();
+}
+
+void revmodel::reset()
+{
+	// Tie the components to their buffers
+	int i, tuning;
+	for (i=0;i<numcombs;i++) {
+		tuning = resetallpassbuf((float *)bufcombL[i], i, false);
+		combL[i].setbuffer(bufcombL[i], tuning);
+
+		tuning = resetallpassbuf((float *)bufcombR[i], i, true);
+		combR[i].setbuffer(bufcombR[i], tuning);
+	}
+	for (i=0;i<numallpasses;i++) {
+		tuning = resetallpassbuf((float *)bufallpassL[i], i, false);
+		allpassL[i].setbuffer(bufallpassL[i], tuning);
+
+		tuning = resetallpassbuf((float *)bufallpassR[i], i, true);
+		allpassR[i].setbuffer(bufallpassR[i], tuning);
+	}
+}
+
+int revmodel::resetcombbuf(float *buf, int i, bool stereo)
+{
+	int tuning = gettuning(i, (int *)combtuning, numcombtuning, stereo);
+	if (buf)
+		delete buf;
+	buf = (float *)std::malloc(tuning * sizeof(float));
+	return tuning;
+}
+
+int revmodel::resetallpassbuf(float *buf, int i, bool stereo)
+{
+	int tuning = gettuning(i, (int *)allpasstuning, numallpasstuning, stereo);
+	if (buf)
+		delete buf;
+	buf = (float *)std::malloc(tuning * sizeof(float));
+	return tuning;
+}
+
+int revmodel::gettuning(int i, int *map, int size, bool stereo)
+{
+	int tuning;
+	if (i < size) {
+		tuning = map[i];
+	} else {
+		tuning = (map[size - 1] - map[size - 2]) * (i - (size - 1)) + map[size - 1];
+	}
+	tuning = tuning * samplerate / 44100;
+	if (stereo)
+		tuning += stereospread;
+	return tuning;
 }
 
 void revmodel::mute()
@@ -266,6 +293,12 @@ float revmodel::getmode()
 
 void revmodel::setstereo(bool value) {
 	stereo = value;
+}
+
+void revmodel::setsamplerate(int value) {
+	samplerate = value;
+	reset();
+	mute();
 }
 
 bool revmodel::getstereo() {
--- Components/revmodel.hpp
+++ Components/revmodel.hpp
@@ -32,7 +32,13 @@ public:
 			float	getmode();
 			void	setstereo(bool value);
 			bool	getstereo();
+			void	setsamplerate(int value);
+			int		getsamplerate();
 private:
+			void	reset();
+			int		resetcombbuf(float *buf, int i, bool stereo);
+			int		resetallpassbuf(float *buf, int i, bool stereo);
+			int		gettuning(int i, int *map, int size, bool stereo);
 			void	update();
 private:
 	float	gain;
@@ -43,10 +49,7 @@ private:
 	float	width;
 	float	mode;
 	bool 	stereo;
-
-	// The following are all declared inline 
-	// to remove the need for dynamic allocation
-	// with its subsequent error-checking messiness
+	int		samplerate;
 
 	// Comb filters
 	comb	combL[numcombs];
@@ -57,32 +60,12 @@ private:
 	allpass	allpassR[numallpasses];
 
 	// Buffers for the combs
-	float	bufcombL1[combtuningL1];
-	float	bufcombR1[combtuningR1];
-	float	bufcombL2[combtuningL2];
-	float	bufcombR2[combtuningR2];
-	float	bufcombL3[combtuningL3];
-	float	bufcombR3[combtuningR3];
-	float	bufcombL4[combtuningL4];
-	float	bufcombR4[combtuningR4];
-	float	bufcombL5[combtuningL5];
-	float	bufcombR5[combtuningR5];
-	float	bufcombL6[combtuningL6];
-	float	bufcombR6[combtuningR6];
-	float	bufcombL7[combtuningL7];
-	float	bufcombR7[combtuningR7];
-	float	bufcombL8[combtuningL8];
-	float	bufcombR8[combtuningR8];
+	float*	bufcombL[numcombs];
+	float*	bufcombR[numcombs];
 
 	// Buffers for the allpasses
-	float	bufallpassL1[allpasstuningL1];
-	float	bufallpassR1[allpasstuningR1];
-	float	bufallpassL2[allpasstuningL2];
-	float	bufallpassR2[allpasstuningR2];
-	float	bufallpassL3[allpasstuningL3];
-	float	bufallpassR3[allpasstuningR3];
-	float	bufallpassL4[allpasstuningL4];
-	float	bufallpassR4[allpasstuningR4];
+	float*	bufallpassL[numallpasses];
+	float*	bufallpassR[numallpasses];
 };
 
 #endif//_revmodel_
